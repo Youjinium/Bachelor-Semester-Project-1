42553ec1c0c5e1f633888dafb4ad18a1b8388840
Youjinium <71777890+Youjinium@users.noreply.github.com>
2020-10-05 09:37:16 +0200

    Add files via upload

---
+++ IntegerDataTypes/IntegerDataTypes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+
+
+            int totalSecondsPlayed = 100;
+            const int SecondsPerMinute = 60;
+
+            int minutesPlayed = totalSecondsPlayed / SecondsPerMinute;
+            int secondsPlayed = totalSecondsPlayed % SecondsPerMinute;
+
+            Console.WriteLine("Minutes Played: " + minutesPlayed);
+            Console.WriteLine("Seconds Played: " + secondsPlayed);
+
+            Console.WriteLine();
+            // Exercise
+            int score = 1360;
+            int played = 10000;
+
+            float pointsPerSecond = score / (float)played;
+            Console.WriteLine("Points per Second: " + pointsPerSecond);
+
+            Console.WriteLine();
+            // Exercise
+            double cosine = (double)Math.Cos(45 * Math.PI / 180);
+            Console.WriteLine("Cosine of 45 degrees: " + cosine);
+
+            Console.WriteLine();
+
+            // Exercise
+            float angle;
+            Console.WriteLine("Enter value of angle");
+            angle = float.Parse(Console.ReadLine());
+
+            angle = (float)Math.PI * 180;
+            Console.WriteLine("Cosine: " + Math.Cos(angle));
+            Console.WriteLine("Sinus: " + Math.Sin(angle));
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Temperature Conversion/Temperature Conversion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conversion from Fahrenheit to Celsius
+            Console.WriteLine("Enter Temperature in Fahrenheit");
+            float fahrenheit = float.Parse(Console.ReadLine());
+
+            float celsius = ((fahrenheit - 32) / 9) * 5;
+         
+            Console.WriteLine("Temperature in Celsius is: " + celsius + "°C");
+            Console.WriteLine();
+
+            //Conversion from Celsius to Fahrenheit
+            Console.WriteLine("Enter Temperature in Celsius");
+            float celsius1 = float.Parse(Console.ReadLine());
+
+            float fahrenheit1 = ((celsius1 * 9) / 5) + 32;
+
+            Console.WriteLine("Temperature in Celsius is: " + fahrenheit1 + "°F");
+            Console.WriteLine();
+
+        }
+    }
+}
